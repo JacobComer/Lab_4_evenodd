be26aabec3708ec5383626f44ff8cdb6cc4a6bb3
jakob <comerjb@mail.uc.edu>
2024-09-30 16:07:31 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 4;
+        if (a % 2 == 0) {
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+
+    }
+    }
